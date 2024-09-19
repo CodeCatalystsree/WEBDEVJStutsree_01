8acb3ee06e749558997edd520555148cb957c1c9
CodeCatalystsree <hatuisrilekha@gmail.com>
2024-09-19 10:42:26 +0530

    Create ass2.js
    js pw assignment-2

---
+++ ass2.js
@@ -0,0 +1,10 @@
+let username="admin";
+let password="12345";
+if(username){
+    if(password){
+        console.log("login successful!");
+    }
+}
+else{
+    console.log("invalid credentials!");
+}
